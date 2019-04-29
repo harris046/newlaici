b936ed218f43617e009306f3b401c2d2392f804b
harris046 <harrisilham0046@gmail.com>
2019-04-30 03:09:41 +0800

    db set laici laici

--- functions/connect.php
+++ functions/connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$password = "";
+$user = "laici";
+$password = "laici";
 $db = "db_elearning";
 
 // $con = mysqli_connect($host,$user,$password) or die("Could not connect to database");
